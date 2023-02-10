13ca3775258ede60b258703e9f338f733e387721
MikhailSivinskikh <mikhail.sivinskikh@bwt.ee>
2023-02-10 16:36:00 +0200

    Updated the workshop repo with new packages and test samples

--- __tests__/api/api.spec.js
+++ __tests__/api/api.spec.js
@@ -3,7 +3,28 @@
  */
 const frisby = require('frisby')
 const data = require('../../helpers/data.js')
-describe('', function () {
-    it('should ', async () => {
+const {Joi} = require("frisby");
+describe('Sample test', function () {
+    let userId;
+    beforeAll(async () => {
+        console.log('before the test', data.genRandomNumber(10, 100))
+        data.authenticate(data.token)
+    })
+    afterAll(async () => {
+        console.log('after the test')
+    })
+    it('Get user collection', async () => {
+        await frisby.get(`https://gorest.co.in/public/v2/users/`)
+            .expect('status', 200)
+            .expect('jsonTypes', '0', {
+                id: Joi.number().required(),
+                name: Joi.string().required(),
+            })
+            .then(res=>{
+                userId = res.json[0].id
+                console.log('First user has an id of:', userId)
+            })
+            .inspectRequest()
+            .inspectResponse()
     });
 });
---
+++ __tests__/api/posts.spec.js
@@ -0,0 +1,24 @@
+/**
+ * @author
+ */
+
+xdescribe('Users CRUD', function () {
+    beforeAll(()=>{
+        //authorize here
+    })
+    it('READ users', async () => {
+
+    });
+    it('POST a new user', async () => {
+
+    });
+    it('GET the created user', async () => {
+
+    });
+    it('PATCH the created user', async () => {
+
+    });
+    it('DELETE the created user', async () => {
+
+    });
+})
